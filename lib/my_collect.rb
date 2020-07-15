f40b95456e032af8c46432099d9f5aadf136197c
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 23:08:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]){|name| name.split(" ").first}
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']){|lang| lang.upcase}
