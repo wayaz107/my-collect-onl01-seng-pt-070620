c14c601a93b8306b52f2edb90298a3913ab37898
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 23:05:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+  i = 0
+  collection = []
+  while i < collection.length
+    collection << yield([i])
+    i += 1
+  end
+  collection
+end
+
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]){|name| name.split("").first}
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']){|lang| lang.upcase}
+
 
 
